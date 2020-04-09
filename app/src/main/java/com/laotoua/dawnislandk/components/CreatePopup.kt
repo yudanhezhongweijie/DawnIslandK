15cc7ea80c5bd3aa24f6d86855075032a5c3720b
Rick <weijie.zhong@outlook.com>
2020-04-09 15:43:35 -0400

    added cookie list selection in create popup

--- app/src/main/java/com/laotoua/dawnislandk/components/CreatePopup.kt
+++ app/src/main/java/com/laotoua/dawnislandk/components/CreatePopup.kt
@@ -9,15 +9,25 @@ import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.invoke
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.laotoua.dawnislandk.R
+import com.laotoua.dawnislandk.entities.Cookie
+import com.laotoua.dawnislandk.util.AppState
+import com.lxj.xpopup.XPopup
+import com.lxj.xpopup.core.BasePopupView
 import com.lxj.xpopup.core.BottomPopupView
 import com.lxj.xpopup.util.XPopupUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import timber.log.Timber
+
 
 @SuppressLint("ViewConstructor")
 class CreatePopup(private val caller: Fragment, context: Context) :
@@ -34,6 +44,33 @@ class CreatePopup(private val caller: Fragment, context: Context) :
     { uri: Uri? ->
         // TODO Handle the returned Uri
         Timber.i("selected uri: $uri")
+    }
+
+    private var cookies = listOf<Cookie>()
+
+    private var selectedCookie: String? = null
+
+    override fun show(): BasePopupView {
+        caller.lifecycleScope.launch {
+            loadCookies()
+        }
+        return super.show()
+    }
+
+    private suspend fun loadCookies() {
+        withContext(Dispatchers.IO) {
+            AppState.loadCookies()
+            cookies = AppState.cookies!!
+        }
+        if (selectedCookie == null || cookies.isNullOrEmpty()) {
+            findViewById<TextView>(R.id.cookie)?.let {
+                it.text = if (cookies.isNullOrEmpty()) {
+                    "没有饼干"
+                } else {
+                    cookies[0].userHash
+                }
+            }
+        }
     }
 
     private fun checkPermission(context: Context) {
@@ -74,12 +111,10 @@ class CreatePopup(private val caller: Fragment, context: Context) :
 
         findViewById<ImageButton>(R.id.doodle).setOnClickListener {
             // TODO
-            Timber.i("Clicked on doodle")
+            Toast.makeText(caller.context, "还没做。。。", Toast.LENGTH_SHORT).show()
         }
 
         findViewById<ImageButton>(R.id.expandMore).setOnClickListener {
-            Timber.i("Clicked on expandMore")
-
             findViewById<LinearLayout>(R.id.expansion).let {
                 if (it.visibility == View.VISIBLE) {
                     it.visibility = View.GONE
@@ -90,8 +125,25 @@ class CreatePopup(private val caller: Fragment, context: Context) :
         }
 
         findViewById<TextView>(R.id.cookie).setOnClickListener {
-            Timber.i("Clicked on cookie")
-            // TODO
+
+            if (!cookies.isNullOrEmpty()) {
+                XPopup.Builder(context)
+                    .atView(it) // 依附于所点击的View，内部会自动判断在上方或者下方显示
+                    .asAttachList(
+                        cookies.map { c -> c.userHash }.toTypedArray(),
+                        intArrayOf(
+                            R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher,
+                            R.mipmap.ic_launcher, R.mipmap.ic_launcher
+                        )
+                    ) { _, text ->
+                        selectedCookie = text
+                        findViewById<TextView>(R.id.cookie).text = selectedCookie
+                    }
+                    .show()
+            } else {
+                Toast.makeText(caller.context, "没有饼干", Toast.LENGTH_SHORT).show()
+            }
         }
     }
+
 }

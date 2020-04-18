812dc6f7edbc9ee0021fc803e877cad5a3605509
Rick <weijie.zhong@outlook.com>
2020-04-17 22:10:32 -0400

    added photo preview in post checked compatibility on API23 and API 29

---
+++ app/src/main/java/com/laotoua/dawnislandk/util/ImageUtil.kt
@@ -0,0 +1,84 @@
+package com.laotoua.dawnislandk.util
+
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.provider.OpenableColumns
+import android.util.Size
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+
+object ImageUtil {
+    fun addPlaceholderImageUriToGallery(
+        caller: Fragment,
+        fileName: String,
+        fileExt: String,
+        relativeLocation: String
+    ): Uri? {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.ImageColumns.DISPLAY_NAME, "$fileName.$fileExt")
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/$fileExt")
+
+            // without this part causes "Failed to create new MediaStore record" exception to be invoked (uri is null below)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.Images.ImageColumns.RELATIVE_PATH, relativeLocation)
+            }
+        }
+        return caller.requireActivity().contentResolver.insert(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        )
+            ?: throw IOException("Failed to create new MediaStore record.")
+    }
+
+    fun loadImageThumbnailToImageView(
+        caller: Fragment,
+        uri: Uri,
+        width: Int,
+        height: Int,
+        imageView: ImageView
+    ) {
+        if (Build.VERSION.SDK_INT >= 29) {
+            caller.requireActivity().contentResolver
+                .loadThumbnail(uri, Size(width, height), null)
+                .run { imageView.setImageBitmap(this) }
+        } else {
+            GlideApp.with(caller).load(uri).override(width, height).into(imageView)
+        }
+    }
+
+    fun getImagePathFromUri(caller: Fragment, uri: Uri): File? {
+        val parcelFileDescriptor =
+            caller.requireActivity().contentResolver.openFileDescriptor(uri, "r", null)
+
+        parcelFileDescriptor?.let {
+            val inputStream = FileInputStream(parcelFileDescriptor.fileDescriptor)
+            val file = File(
+                caller.requireContext().cacheDir,
+                caller.requireContext().contentResolver.getFileName(uri)
+            )
+            val outputStream = FileOutputStream(file)
+            inputStream.copyTo(outputStream)
+            return file
+        }
+        return null
+    }
+
+    private fun ContentResolver.getFileName(fileUri: Uri): String {
+        var name = ""
+        val returnCursor = this.query(fileUri, null, null, null, null)
+        if (returnCursor != null) {
+            val nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+            returnCursor.moveToFirst()
+            name = returnCursor.getString(nameIndex)
+            returnCursor.close()
+        }
+        return name
+    }
+}

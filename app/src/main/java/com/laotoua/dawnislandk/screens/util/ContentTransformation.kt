a9b59621171ec3eea0a88e056362179c8e9d23dd
Rick <weijie.zhong@outlook.com>
2020-07-04 15:04:47 -0400

    fix #188

--- app/src/main/java/com/laotoua/dawnislandk/screens/util/ContentTransformation.kt
+++ app/src/main/java/com/laotoua/dawnislandk/screens/util/ContentTransformation.kt
@@ -41,6 +41,10 @@ object ContentTransformation {
         Pattern.compile("(http|https)://[a-z0-9A-Z%-]+(\\.[a-z0-9A-Z%-]+)+(:\\d{1,5})?(/[a-zA-Z0-9-_~:#@!&',;=%/*.?+$\\[\\]()]+)?/?")
     private val AC_PATTERN = Pattern.compile("ac\\d+")
     private val HIDE_PATTERN = Pattern.compile("\\[h](.+?)\\[/h]")
+    private const val CUSTOM_HIDE_PATTERN_OPEN = "<-hide->"
+    private const val CUSTOM_HIDE_PATTERN_CLOSE = "</-hide->"
+    private val CUSTOM_HIDE_PATTERN =
+        Pattern.compile("$CUSTOM_HIDE_PATTERN_OPEN(.+?)$CUSTOM_HIDE_PATTERN_CLOSE")
 
     @Suppress("DEPRECATION")
     fun htmlToSpanned(string: String): Spanned {
@@ -69,7 +73,12 @@ object ContentTransformation {
             cookie.setSpan(poColor, 0, cookie.length, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
         }
         if (userId == po) {
-            cookie.setSpan(StyleSpan(Typeface.BOLD), 0, cookie.length, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+            cookie.setSpan(
+                StyleSpan(Typeface.BOLD),
+                0,
+                cookie.length,
+                Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+            )
         }
         return cookie
     }
@@ -85,11 +94,37 @@ object ContentTransformation {
         referenceClickListener: ReferenceSpan.ReferenceClickHandler? = null
     ): SpannableStringBuilder {
         return SpannableStringBuilder(htmlToSpanned(content))
+            .replaceHideTag()
             .handleTextUrl()
             .handleReference(context, referenceClickListener)
             .handleAcUrl()
             .handleHideTag()
             .handleLineHeightAndSegGap(lineHeight, segGap)
+    }
+
+    private fun SpannableStringBuilder.replaceHideTag() = apply {
+        val m: Matcher = HIDE_PATTERN.matcher(this)
+        // surrounding [h][/h]
+        val openTagLen = 3
+        val closeTagLen = 4
+        val newOpenTagLen = CUSTOM_HIDE_PATTERN_OPEN.length
+        val newCloseTagLen = CUSTOM_HIDE_PATTERN_CLOSE.length
+        val step = openTagLen + closeTagLen - newOpenTagLen - newCloseTagLen
+        var matchCount = 0
+        while (m.find()) {
+            val start = m.start() - step * matchCount
+            val end = m.end() - step * matchCount
+            //  replace surrounding [h][/h] with new custom tag
+            replace(
+                start,
+                end,
+                CUSTOM_HIDE_PATTERN_OPEN + this.subSequence(
+                    start + openTagLen,
+                    end - closeTagLen
+                ) + CUSTOM_HIDE_PATTERN_CLOSE
+            )
+            matchCount += 1
+        }
     }
 
     private fun SpannableStringBuilder.handleLineHeightAndSegGap(lineHeight: Int, segGap: Int) =
@@ -104,7 +139,8 @@ object ContentTransformation {
             )
         }
 
-    private fun SpannableStringBuilder.handleReference(context: Context,
+    private fun SpannableStringBuilder.handleReference(
+        context: Context,
         referenceClickListener: ReferenceSpan.ReferenceClickHandler? = null
     ) = apply {
         if (referenceClickListener != null) {
@@ -172,17 +208,20 @@ object ContentTransformation {
     }
 
     private fun SpannableStringBuilder.handleHideTag() = apply {
-        val m: Matcher = HIDE_PATTERN.matcher(this)
+        val m: Matcher = CUSTOM_HIDE_PATTERN.matcher(this)
+        val openTagLen = CUSTOM_HIDE_PATTERN_OPEN.length
+        val closeTagLen = CUSTOM_HIDE_PATTERN_CLOSE.length
+        val step = openTagLen + closeTagLen
         var matchCount = 0
         while (m.find()) {
-            val start = m.start() - 7 * matchCount
-            val end = m.end() - 7 * matchCount
+            val start = m.start() - step * matchCount
+            val end = m.end() - step * matchCount
             //  remove surrounding [h][/h]
-            replace(start, end, this.subSequence(start + 3, end - 4))
+            replace(start, end, this.subSequence(start + openTagLen, end - closeTagLen))
 
-            val hideSpan = HideSpan(start, end - 7)
-            setSpan(hideSpan, start, end - 7, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-            hideSpan.hideSecret(this, start, end - 7)
+            val hideSpan = HideSpan(start, end - step)
+            setSpan(hideSpan, start, end - step, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            hideSpan.hideSecret(this, start, end - step)
             matchCount += 1
         }
     }
